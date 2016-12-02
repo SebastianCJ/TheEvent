c3ecaeec55f3a123e6690ed499a871eafc730ebd
SebastianCJ <cejudosebastian@gmail.com>
2016-12-02 14:03:58 -0800

    Ultimos Cambios
    Adapter Event

--- app/src/main/java/com/example/gatsu/theevent/Evento.java
+++ app/src/main/java/com/example/gatsu/theevent/Evento.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -16,58 +18,49 @@ public class Evento extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evento);
 
-        final RelativeLayout eventContainer = (RelativeLayout) findViewById(R.id.eventContainer);
+        ListView eventContainer = (ListView) findViewById(R.id.eventContainer);
         TextView selectEvento = (TextView) this.findViewById(R.id.seleccionaEvento);
         TextView misEventos = (TextView) this.findViewById(R.id.misEventos);
+        ImageView agregarbtn = (ImageView) this.findViewById(R.id.btnagregarimg);
         selectEvento.setTypeface(CustomFontsLoader.getTypeface(this,CustomFontsLoader.Bold));
         misEventos.setTypeface(CustomFontsLoader.getTypeface(this,CustomFontsLoader.Light));
 
-        int numEventos = 0;
+        String[] nombres = new String[] { "Amaya y Juan", "Amaya y Juan", "Amaya y Juan"
+                 };
 
-        while(numEventos < 3) {
-            ImageView imgEvento = new ImageView(this);
-            imgEvento.setId(numEventos+1);
-            imgEvento.setImageResource(R.mipmap.eventos323);
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(dpiTopx(15), dpiTopx(10), dpiTopx(15), 0);
-            params.addRule(RelativeLayout.BELOW, numEventos);
-            eventContainer.addView(imgEvento,params);
-            numEventos++;
-        }
+        String[] lugares = new String[] { "Nombre del Lugar", "Nombre del Lugar", "Nombre del Lugar"
+                 };
 
-        ImageView agregar = new ImageView(this);
-        agregar.setId(numEventos+1);
-        agregar.setImageResource(R.mipmap.btnagregarevento);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        params.setMargins(0, dpiTopx(25), 0, 0);
-        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        params.addRule(RelativeLayout.BELOW, numEventos);
-        eventContainer.addView(agregar,params);
-        agregar.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(Evento.this, Camera.class);
+        String[] descripciones = new String[] { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+                 };
+
+        String[] fechas = new String[] { "12/Marzo/2017", "13/Marzo/2017", "12/Abril/2017"
+                 };
+
+        AdapterEventos adapter = new AdapterEventos(this, nombres,lugares,descripciones,fechas);
+        eventContainer.setAdapter(adapter);
+
+        eventContainer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                Intent intent = new Intent(Evento.this, Login.class);
                 startActivity(intent);
-
             }
+
         });
 
-        TextView agregarTxt = new TextView(this);
-        RelativeLayout.LayoutParams paramsTxt = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        paramsTxt.setMargins(0, dpiTopx(15), 0, dpiTopx(15));
-        paramsTxt.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        paramsTxt.addRule(RelativeLayout.BELOW, agregar.getId());
-        System.out.println("AGREGAR ID: "+agregar.getId());
-        agregarTxt.setTextSize(TypedValue.COMPLEX_UNIT_SP,14);
-        agregarTxt.setText(R.string.agregarEvento);
-        eventContainer.addView(agregarTxt,paramsTxt);
+        agregarbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Evento.this, Camera.class);
+                startActivity(intent);
+            }
+
+        });
+    }
     }
 
-    private int dpiTopx(float dips)
-    {
-        float DP = getResources().getDisplayMetrics().density;
-        return Math.round(dips * DP);
-    }
-}
